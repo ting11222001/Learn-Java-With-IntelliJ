5b06fde26128cbcf73a8ba3fd434933d4da2c596
tiffany <ting11222001@gmail.com>
2024-07-16 12:59:06 +0930

    practice some intellij shortcuts and generate code templates

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -6,12 +6,16 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome!"); // Hello and welcome!
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println("i = " + i); // i = 1, i = 2, etc.
         }
+
+        Product product = new Product("Keyboard");
+        System.out.println(product.getName()); // Keyboard
+        System.out.println(product.toString()); // Product{name='Keyboard'}
     }
 }
